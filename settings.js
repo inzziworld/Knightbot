ee8b54d0cf39b92deb2b55c6863ef28d347527dd
Inzziworld <74729983+inzziworld@users.noreply.github.com>
2025-05-31 16:01:37 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Inzzitech', // Your name
-  ownerNumber: '2347084039885', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2348087557423', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
